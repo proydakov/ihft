a9b6f20bfc911ff8db484bdc445c8b9ac0fa31c1
Evgeny Proydakov <e.proydakov@gmail.com>
2020-06-18 19:00:15 +0300

    Formatting.

--- platform/include/platform/platform.h
+++ platform/include/platform/platform.h
@@ -2,16 +2,18 @@
 
 namespace ihft
 {
-    class platform
-    {
-        public:
-            // Change current thread name. Could be useful with htop
-            // The name can be up to 16 bytes long, including the terminating null byte.
-            // (If the length of the string, including the terminating null byte,
-            // exceeds 16 bytes, the string is silently truncated.)
-            static void set_current_thread_name(const char* name);
 
-            // Change current thread cpu.
-            static void set_current_thread_cpu(unsigned long cpu);
-    };
+class platform
+{
+public:
+    // Change current thread name. Could be useful with htop
+    // The name can be up to 16 bytes long, including the terminating null byte.
+    // (If the length of the string, including the terminating null byte,
+    // exceeds 16 bytes, the string is silently truncated.)
+    static void set_current_thread_name(const char* name);
+
+    // Change current thread cpu
+    static void set_current_thread_cpu(unsigned long cpu);
+};
+
 }
