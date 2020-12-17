aa17c25efa7b5b83d18ac5dfda7481f7b997522e
Evgeny Proydakov <e.proydakov@gmail.com>
2020-12-18 00:15:56 +0300

    Removed one2many_bitmask_queue

--- channel/test/test_bitmask_queue_latency.cpp
+++
@@ -1,8 +0,0 @@
-#include <main.h>
-#include <test_latency.h>
-#include <channel/one2many_bitmask_queue.h>
-
-int main(int argc, char* argv[])
-{
-    return test_main<one2many_bitmask_queue<data_t, std::uint64_t>, latency_test>(argc, argv);
-}
