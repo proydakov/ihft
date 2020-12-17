ec4303d2d16721b4c103e852d81a963a61d9c9ee
Evgeny Proydakov <e.proydakov@gmail.com>
2020-12-18 00:16:28 +0300

    Review one2many counter queue to2pow.

--- channel/include/channel/one2many_counter_queue.h
+++ channel/include/channel/one2many_counter_queue.h
@@ -17,7 +17,7 @@ struct one2many_counter_queue_impl
     enum : T { MIN_EVENT_SEQ_NUM = 1 };
     enum : T { DUMMY_EVENT_SEQ_NUM = 0 };
     enum : T { MIN_READER_ID = 0 };
-    enum : T { DUMMY_READER_ID = std::numeric_limits<T>::max() };
+    enum : T { DUMMY_READER_ID = 4096 };
     enum : T { EMPTY_DATA_MARK = 0 };
     enum : T { CONSTRUCTED_DATA_MARK = 1 };
 };
@@ -151,11 +151,11 @@ public:
         static_assert(sizeof(one2many_counter_reader<event_t, counter_t, content_allocator_t>) <= QUEUE_CPU_CACHE_LINE_SIZE);
     }
 
+    one2many_counter_reader(one2many_counter_reader&&) noexcept = default;
+
+    one2many_counter_reader& operator=(one2many_counter_reader&&) noexcept = delete;
     one2many_counter_reader(const one2many_counter_reader&) = delete;
     one2many_counter_reader& operator=(const one2many_counter_reader&) = delete;
-
-    one2many_counter_reader(one2many_counter_reader&&) noexcept = default;
-    one2many_counter_reader& operator=(one2many_counter_reader&&) noexcept = default;
 
     ~one2many_counter_reader() noexcept
     {
@@ -217,7 +217,7 @@ public:
         m_storage.reset(new bucket_type[n], [](bucket_type* ptr){
             delete [] ptr;
         });
-        m_storage_mask = calc_mask(n);
+        m_storage_mask = n - 1;
     }
 
     one2many_counter_queue(const one2many_counter_queue&) = delete;
@@ -283,17 +283,18 @@ private:
             return 1;
         }
 
+        constexpr auto max_size = std::size_t(2) << 31;
+        if (n >= max_size)
+        {
+            return max_size;
+        }
+
         std::size_t power = 1;
         while(power < n)
         {
             power *= 2;
         }
         return power;
-    }
-
-    static std::size_t calc_mask(std::size_t n) noexcept
-    {
-        return n - 1;
     }
 
 private:
