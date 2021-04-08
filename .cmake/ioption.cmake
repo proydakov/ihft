99c8ca19af97b76083a463f77142246524e28bce
Evgeny Proydakov <e.proydakov@gmail.com>
2021-04-08 22:53:27 +0300

    Fixed cmake for linux memory sanitizer.

--- .cmake/ioption.cmake
+++ .cmake/ioption.cmake
@@ -1,4 +1,5 @@
 option(IHFT_STATIC_LINK "Use static runtime for linkage. Compile dependency free executables." OFF)
 option(IHFT_STRIP_LINK  "Strip binaries in linker stage." OFF)
-option(IHFT_SANITIZE_LINK "Using address sanitizers for binary." OFF)
+option(IHFT_ADDRESS_SANITIZER "Using address sanitizers for binary." OFF)
+option(IHFT_MEMORY_SANITIZER "Using memory sanitizers for binary." OFF)
 option(IHFT_BUILD_UNITTESTS "Build unit tests for project" ON)
