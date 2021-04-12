5bbb45c63315cd133f8e34e7e219523b4ea3b135
Evgeny Proydakov <e.proydakov@gmail.com>
2021-04-12 11:11:20 +0300

    Updated Latency Hist python script

--- analytics/latency_hist.py
+++ analytics/latency_hist.py
@@ -7,9 +7,12 @@ x = np.loadtxt(sys.argv[1], delimiter="\n")
 q = [50, 75, 80, 95, 99, 99.9, 100]
 p = np.percentile(x, q = q)
 
-plt.hist(x, density=True, bins=1024, label=['Eastasia', 'Eurasia', 'Oceania'])  # density=False would make counts
-plt.ylabel('Probability')
-plt.xlabel('Data (nanoseconds)');
+fig, ax = plt.subplots()
+fig.canvas.set_window_title('Latency hist for: ' + sys.argv[1])
+
+plt.hist(x, density=True, bins=1024)  # density=False would make counts
+plt.ylabel('Samples')
+plt.xlabel('Latency (nanoseconds)');
 
 for i in range(len(q)):
     perc = q[i]
