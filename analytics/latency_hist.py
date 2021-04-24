e950c39111f37a6b28f1b006ce3f2e5293e21efb
Evgeny Proydakov <e.proydakov@gmail.com>
2021-04-25 00:24:22 +0300

    Supported many files for latency analyzation

--- analytics/latency_hist.py
+++ analytics/latency_hist.py
@@ -2,21 +2,25 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.loadtxt(sys.argv[1], delimiter="\n")
+for x in range(1, len(sys.argv)):
+    fname = sys.argv[x]
 
-q = [50, 75, 80, 95, 99, 99.9, 100]
-p = np.percentile(x, q = q)
+    x = np.loadtxt(fname, delimiter="\n")
 
-fig, ax = plt.subplots()
-fig.canvas.set_window_title('Latency hist for: ' + sys.argv[1])
+    q = [50, 75, 80, 95, 99, 99.9, 100]
+    p = np.percentile(x, q = q)
 
-plt.hist(x, density=True, bins=1024)  # density=False would make counts
-plt.ylabel('Samples')
-plt.xlabel('Latency (nanoseconds)');
+    fig, ax = plt.subplots()
+    fig.canvas.set_window_title('Latency hist for: ' + fname)
 
-for i in range(len(q)):
-    perc = q[i]
-    cycl = p[i]
-    print(f"percentile[{perc}]: {cycl}")
+    print(f"file: {fname}")
+    plt.hist(x, density=True, bins=1024)  # density=False would make counts
+    plt.ylabel('Samples')
+    plt.xlabel('Latency (nanoseconds)');
+
+    for i in range(len(q)):
+        perc = q[i]
+        cycl = p[i]
+        print(f"percentile[{perc}]: {cycl}")
 
 plt.show()
