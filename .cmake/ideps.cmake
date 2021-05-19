ef50a0641c54e7669dfe804dc85995f5dbd935cd
Evgeny Proydakov <e.proydakov@gmail.com>
2021-05-19 11:59:06 +0300

    Using tar.gz & zip dependencies instead of git.

--- .cmake/ideps.cmake
+++ .cmake/ideps.cmake
@@ -1,12 +1,14 @@
-if (IHFT_BUILD_UNITTESTS)
-    include(FetchContent)
+message(STATUS "Downloading dependencies...")
 
+include(FetchContent)
+#set(FETCHCONTENT_QUIET FALSE)
+
+if (IHFT_BUILD_UNITTESTS)
     FetchContent_Declare(
         Catch2
-        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-        GIT_TAG        v2.13.6
+        URL https://github.com/catchorg/Catch2/archive/refs/tags/v2.13.6.tar.gz
+        URL_HASH MD5=c7c7ef181b9e08418fd9f2ef8159d03f
     )
-
     FetchContent_GetProperties(Catch2)
     if(NOT Catch2_POPULATED)
         FetchContent_Populate(Catch2)
@@ -16,14 +18,11 @@ if (IHFT_BUILD_UNITTESTS)
 endif()
 
 if (IHFT_BUILD_TOML)
-    include(FetchContent)
-
     FetchContent_Declare(
         tomlplusplus
-        GIT_REPOSITORY https://github.com/marzer/tomlplusplus.git
-        GIT_TAG        0fcbfbe655917738c39321fcbcbdc7b048a40e33
+        URL https://github.com/marzer/tomlplusplus/archive/a29ecda10263d49cd79864380659b413e7f27411.zip
+        URL_HASH MD5=aec5863885cbdc6d17631576abb07024
     )
-
     FetchContent_GetProperties(tomlplusplus)
     if(NOT tomlplusplus_POPULATED)
         FetchContent_Populate(tomlplusplus)
