c8ade4123fbe60b2eb5341673601663a00870723
Evgeny Proydakov <e.proydakov@gmail.com>
2022-01-24 14:12:24 +0300

    Updated toml_demo. Added toml_doc in misc examples.

--- misc/example/toml_demo.cpp
+++ misc/example/toml_demo.cpp
@@ -1,21 +1,11 @@
-#define TOML_EXCEPTIONS 0
+#include "toml_doc.h"
 
+#define TOML_EXCEPTIONS 0
 #include <toml++/toml.h>
 
 #include <fstream>
 #include <iostream>
 #include <string_view>
-
-const char * const TOML = R"(
-[library]
-name = "toml++"
-authors = ["Mark Gillard <mark.gillard@outlook.com.au>"]
-cities = ["上海", "北京", "深圳"]
-bogatyrs = ["Добрыня Никитич", "Илья Муромец", "Алёша Попович"]
-
-[dependencies]
-cpp = 17
-)";
 
 const char * const FNAME = "configuration.toml";
 
@@ -28,6 +18,18 @@ int main()
 
     auto config = toml::parse_file( FNAME );
 
+    if (config.failed())
+    {
+        std::cerr << config.error() << "\n";
+        return 1;
+    }
+
+    std::cout << "sizeof(toml::parse_result): " << sizeof(config) << "\n";
+    std::cout << "alignof(toml::parse_result): " << alignof(decltype(config)) << "\n";
+
+    std::cout << "sizeof(toml::table): " << sizeof(toml::table) << "\n";
+    std::cout << "alignof(toml::table): " << alignof(toml::table) << "\n";
+
     // get key-value pairs
     std::string_view library_name = config["library"]["name"].value_or(std::string_view(""));
     if (config["library"]["authors"].is_array())
@@ -35,7 +37,7 @@ int main()
         std::cout << "len: " << config["library"]["authors"].as_array()->size() << "\n";
     }
     std::string_view library_author = config["library"]["authors"][0].value_or(std::string_view(""));
-    int64_t depends_on_cpp_version = config["dependencies"]["cpp"].value_or(0);
+    int64_t depends_on_cpp_version = config["dependencies"]["lang.cpp"].value_or(0);
 
     std::cout << "library_name: " << library_name << "\n";
     std::cout << "library_author: " << library_author << "\n";
@@ -46,10 +48,12 @@ int main()
     // iterate & visit over the data
     for (auto const& [k, v] : config)
     {
+        std::cout << "key: '" << k << "'\n";
         v.visit([](auto& node) noexcept
         {
             std::cout << node << "\n";
         });
+        std::cout << '\n';
     }
 
     std::cout << "\nTOML:\n\n";
---
+++ misc/example/toml_doc.h
@@ -0,0 +1,15 @@
+#pragma once
+
+const char * const TOML =
+R"([library]
+name = "toml++"
+authors = ["Mark Gillard <mark.gillard@outlook.com.au>"]
+cities = ["上海", "北京", "深圳"]
+bogatyrs = ["Добрыня Никитич", "Илья Муромец", "Алёша Попович"]
+
+[dependencies]
+lang = "C++"
+version = 20
+compiler = "clang"
+operation_system = "linux"
+)";
