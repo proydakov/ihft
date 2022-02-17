9480d735631411dda68bb4a267384d154e99c381
Evgeny Proydakov <e.proydakov@gmail.com>
2022-02-17 10:45:01 +0300

    Converted engine logical cpu into template + added a new checks in unittest

--- engine/include/engine/logical_cpu.h
+++ engine/include/engine/logical_cpu.h
@@ -1,23 +1,37 @@
 #pragma once
 
+#include <platform/platform.h>
+
 #include <string>
 #include <string_view>
 
 namespace ihft::engine
 {
 
-class logical_cpu final
+template<typename platform>
+class logical_cpu_impl final
 {
 public:
-    logical_cpu(unsigned cpu_id, std::string cpu_name) noexcept;
+    logical_cpu_impl(unsigned cpu_id, std::string cpu_name) noexcept
+        : m_binded(false)
+        , m_cpu_id(cpu_id)
+        , m_cpu_name(std::move(cpu_name))
+    {
+    }
 
-    ~logical_cpu();
+    ~logical_cpu_impl()
+    {
+        if (m_binded)
+        {
+            m_binded = !platform::reset_current_thread_cpu();
+        }
+    }
 
-    logical_cpu(logical_cpu&& other) noexcept = delete;
-    logical_cpu& operator=(logical_cpu&& other) noexcept = delete;
+    logical_cpu_impl(logical_cpu_impl&& other) noexcept = delete;
+    logical_cpu_impl& operator=(logical_cpu_impl&& other) noexcept = delete;
 
-    logical_cpu(logical_cpu const&) noexcept = delete;
-    logical_cpu& operator=(logical_cpu const&) noexcept = delete;
+    logical_cpu_impl(logical_cpu_impl const&) noexcept = delete;
+    logical_cpu_impl& operator=(logical_cpu_impl const&) noexcept = delete;
 
     unsigned get_id() const noexcept
     {
@@ -30,12 +44,18 @@ public:
     }
 
     // bind logical cpu to system cpu, also change system thread name
-    bool bind() noexcept;
+    bool bind() noexcept
+    {
+        return m_binded = platform::set_current_thread_cpu(m_cpu_id)
+            && platform::set_current_thread_name(m_cpu_name.c_str());
+    }
 
 private:
     bool m_binded;
     unsigned m_cpu_id;
     std::string m_cpu_name;
 };
+
+using logical_cpu = logical_cpu_impl<ihft::platform::trait>;
 
 }
--- engine/test/test_logical_cpu.cpp
+++ engine/test/test_logical_cpu.cpp
@@ -2,10 +2,53 @@
 
 #include <engine/logical_cpu.h>
 
+namespace
+{
+    static unsigned g_cpu{};
+    static std::string g_cpu_name{};
+
+    struct test_platform
+    {
+        static bool set_current_thread_cpu(unsigned cpu)
+        {
+            g_cpu = cpu;
+
+            return true;
+        }
+
+        static bool set_current_thread_name(const char * const name)
+        {
+            g_cpu_name = name;
+
+            return true;
+        }
+
+        static bool reset_current_thread_cpu()
+        {
+            g_cpu = 0;
+
+            return true;
+        }
+    };
+}
+
 TEST_CASE("logical_cpu")
 {
-    ihft::engine::logical_cpu cpu(0, "test");
-    REQUIRE(cpu.get_id() == 0);
-    REQUIRE(cpu.get_name() == "test");
-    REQUIRE(cpu.bind());
+    {
+        using test_logical_cpu = ihft::engine::logical_cpu_impl<test_platform>;
+
+        test_logical_cpu cpu(7, "omega");
+        REQUIRE(cpu.get_id() == 7);
+        REQUIRE(cpu.get_name() == "omega");
+
+        REQUIRE(g_cpu == 0);
+        REQUIRE(g_cpu_name == "");
+
+        REQUIRE(cpu.bind());
+
+        REQUIRE(g_cpu == 7);
+        REQUIRE(g_cpu_name == "omega");
+    }
+
+    REQUIRE(g_cpu == 0);
 }
