f63506e619c9d85c9e66ae43cf6f657b17d5ed47
Evgeny Proydakov <e.proydakov@gmail.com>
2022-02-19 12:29:05 +0300

    Updated platform sysjitter :
    1) Added option `-d` for saving results
    2) Convert help option `-h` into flag
    3) The code is reorganized by functions

--- platform/example/sysjitter.cpp
+++ platform/example/sysjitter.cpp
@@ -17,6 +17,8 @@
 #include <vector>
 #include <iomanip>
 #include <cstdlib>
+#include <fstream>
+#include <optional>
 #include <iostream>
 #include <algorithm>
 
@@ -26,64 +28,44 @@ using cclock_t = std::chrono::steady_clock;
 using nanosec_t = std::chrono::nanoseconds;
 using samples_t = std::vector<nanosec_t>;
 using time_point_t = std::chrono::time_point<cclock_t>;
+using resultvec_t = std::vector<samples_t>;
+using sync_t = std::atomic<size_t>;
+
+struct options_t
+{
+    std::chrono::seconds runtime = std::chrono::seconds(5);
+    nanosec_t threshold = nanosec_t::max();
+    size_t nsamples = static_cast<size_t>(1 << 30) / sizeof(nanosec_t);
+    bool dump = false;
+};
 
 void help();
-
 nanosec_t calculate_default_threshold();
+std::optional<int> parse_options(const int argc, char* const argv[], options_t& opt);
+bool measure(unsigned cpu, size_t cores, sync_t& active, resultvec_t& results, nanosec_t threshold, time_point_t until);
 
 int main(const int argc, char* argv[])
 {
     std::ios::sync_with_stdio(false);
 
-    auto runtime = std::chrono::seconds(5);
-    auto threshold = nanosec_t::max();
-    size_t nsamples = static_cast<size_t>(1 << 30) / sizeof(nanosec_t);
-
-    bool invalid_options = false;
-
-    int opt{};
-    while ((opt = getopt(argc, argv, "r:t:s:h:")) != -1) {
-        switch (opt) {
-            case 'r':
-                runtime = std::chrono::seconds(std::stoul(optarg));
-                break;
-
-            case 't':
-                threshold = nanosec_t(std::stoul(optarg));
-                break;
-
-            case 's':
-                nsamples = std::stoul(optarg);
-                break;
-
-            case 'h':
-                help();
-                return EXIT_SUCCESS;
-                break;
-
-            default:
-                invalid_options = true;
-                break;
-        }
-    }
-
-    if (invalid_options)
+    options_t param;
+    if (auto const res = parse_options(argc, argv, param))
     {
         help();
-        return EXIT_FAILURE;
+        return *res;
     }
 
-    threshold = nanosec_t::max() == threshold ? calculate_default_threshold() : threshold;
+    param.threshold = nanosec_t::max() == param.threshold ? calculate_default_threshold() : param.threshold;
 
     std::size_t const cores = std::thread::hardware_concurrency();
 
     std::vector<std::thread> threads;
     threads.reserve(cores);
 
-    std::vector<samples_t> results(cores);
+    resultvec_t results(cores);
     for (auto& vec : results)
     {
-        vec.reserve(nsamples);
+        vec.reserve(param.nsamples);
     }
 
     std::vector<std::atomic<bool>> valid_experiment(cores);
@@ -94,67 +76,19 @@ int main(const int argc, char* argv[])
             "or run with CAP_IPC_LOC capability.\n";
     }
 
-    std::atomic<size_t> active_threads = 0;
+    sync_t active_threads = 0;
 
-    auto functor = [](unsigned cpu, size_t cores, std::atomic<size_t>& active, std::vector<samples_t>& results, nanosec_t threshold, time_point_t until)
-    {
-        auto& output = results[cpu];
-
-        if (!ihft::platform::trait::set_current_thread_cpu(cpu))
-        {
-            std::cerr << "can't set thread cpu" << std::endl;
-            return false;
-        }
-
-        std::string const tname("sysjitter_" + std::to_string(cpu));
-        if (!ihft::platform::trait::set_current_thread_name(tname.c_str()))
-        {
-            std::cerr << "can't set thread name" << std::endl;
-            return false;
-        }
-
-        active++;
-
-        while (active.load(std::memory_order_relaxed) != cores);
-
-        auto ts1 = cclock_t::now();
-        while (ts1 < until)
-        {
-            auto const ts2 = cclock_t::now();
-            auto const delta = ts2 - ts1;
-            if (delta < threshold)
-            {
-                ts1 = ts2;
-            }
-            else
-            {
-                if (output.size() == output.capacity()) {
-                    [](unsigned cpu) IHFT_COLD {
-                        auto const str = "WARNING preallocated sample space exceeded for cpu: "
-                            + std::to_string(cpu)
-                            + ", increase threshold or number of samples.\n";
-                        std::cerr << str;
-                    }(cpu);
-                }
-                output.emplace_back(delta);
-                ts1 = cclock_t::now();
-            }
-        }
-
-        return true;
-    };
-
-    auto const until = cclock_t::now() + runtime;
+    auto const until = cclock_t::now() + param.runtime;
 
     for (unsigned i = 1; i < cores; i++)
     {
         threads.emplace_back([&, cpu = i]()
         {
-            valid_experiment[cpu] = functor(cpu, cores, active_threads, results, threshold, until);
+            valid_experiment[cpu] = measure(cpu, cores, active_threads, results, param.threshold, until);
         });
     }
 
-    valid_experiment[0] = functor(0, cores, active_threads, results, threshold, until);
+    valid_experiment[0] = measure(0, cores, active_threads, results, param.threshold, until);
 
     for (auto& t : threads)
         t.join();
@@ -177,11 +111,20 @@ int main(const int argc, char* argv[])
         {
             std::sort(s.begin(), s.end());
             std::cout << cpuof << cpu << " "
-              << thof << threshold.count() << " "
+              << thof << param.threshold.count() << " "
               << offset << s.size() << " "
               << offset << s[static_cast<size_t>(static_cast<double>(s.size()) * 0.99)].count() << " "
               << offset << s[static_cast<size_t>(static_cast<double>(s.size()) * 0.999)].count() << " "
               << offset << (!s.empty() ? s.back().count() : 0);
+
+            if (param.dump)
+            {
+                std::ofstream fstream("sysjitter_" + std::to_string(cpu));
+                for(auto const& v : s)
+                {
+                    fstream << v.count() << "\n";
+                }
+            }
         }
         else
         {
@@ -195,12 +138,63 @@ int main(const int argc, char* argv[])
     return 0;
 }
 
+bool measure(unsigned cpu, size_t cores, sync_t& active, resultvec_t& results, nanosec_t threshold, time_point_t until)
+{
+    auto& output = results[cpu];
+
+    if (!ihft::platform::trait::set_current_thread_cpu(cpu))
+    {
+        std::cerr << "can't set thread cpu" << std::endl;
+        return false;
+    }
+
+    std::string const tname("sysjitter_" + std::to_string(cpu));
+    if (!ihft::platform::trait::set_current_thread_name(tname.c_str()))
+    {
+        std::cerr << "can't set thread name" << std::endl;
+        return false;
+    }
+
+    active++;
+
+    while (active.load(std::memory_order_relaxed) != cores);
+
+    auto ts1 = cclock_t::now();
+    while (ts1 < until)
+    {
+        auto const ts2 = cclock_t::now();
+        auto const delta = ts2 - ts1;
+        if (delta < threshold)
+        {
+            ts1 = ts2;
+        }
+        else
+        {
+            if (output.size() == output.capacity()) {
+                [](unsigned cpu) IHFT_COLD {
+                    auto const str = "WARNING preallocated sample space exceeded for cpu: "
+                        + std::to_string(cpu)
+                        + ", increase threshold or number of samples.\n";
+                    std::cerr << str;
+                }(cpu);
+            }
+            output.emplace_back(delta);
+            ts1 = cclock_t::now();
+        }
+    }
+
+    return true;
+}
+
 void help()
 {
-    std::cout << "platform_get_sysjitter 1.0.0 © 2022 Evgeny Proydakov\n"
-        "https://github.com/proydakov/ihft/tree/master/platform/example\n"
-        "usage: platform_get_jitter [-r runtime_seconds] [-t threshold_nanoseconds] [-s number_of_samples]\n";
+    auto const HELP = ""
+"platform_get_sysjitter 1.1.0 © 2022 Evgeny Proydakov\n"
+"https://github.com/proydakov/ihft/tree/master/platform/example\n"
+"usage: platform_get_jitter [-r runtime_seconds] [-t threshold_nanoseconds]"
+"[-s number_of_samples] [-d (dump results)] [-h (print help)]\n";
 
+    std::cout << HELP;
     std::cout.flush();
 }
 
@@ -224,4 +218,38 @@ nanosec_t calculate_default_threshold()
     threshold *= 8;
 
     return threshold;
+}
+
+std::optional<int> parse_options(const int argc, char* const argv[], options_t& params)
+{
+    int opt{};
+    while ((opt = getopt(argc, argv, "dhr:t:s:")) != -1) {
+        switch (opt) {
+            case 'r':
+                params.runtime = std::chrono::seconds(std::stoul(optarg));
+                break;
+
+            case 't':
+                params.threshold = nanosec_t(std::stoul(optarg));
+                break;
+
+            case 's':
+                params.nsamples = std::stoul(optarg);
+                break;
+
+            case 'd':
+                params.dump = true;
+                break;
+
+            case 'h':
+                return EXIT_SUCCESS;
+                break;
+
+            default:
+                return EXIT_FAILURE;
+                break;
+        }
+    }
+
+    return std::nullopt;
 }
