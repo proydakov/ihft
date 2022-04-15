994bc42b2c503428d2365b2aed04bdb2ac499ce9
Evgeny Proydakov <e.proydakov@gmail.com>
2022-04-15 21:53:31 +0300

    Rename channel/benchmark -> channel/measure

--- channel/measure/measure_one2many_stream_object_queue_heap_stream_allocator.cpp
+++ channel/measure/measure_one2many_stream_object_queue_heap_stream_allocator.cpp
@@ -9,8 +9,5 @@ int main(int argc, char* argv[])
 {
     using allocator_t = ihft::stream_fixed_pool_allocator<std::uint64_t>;
     using queue_t = one2many_seqnum_stream_object_queue<data_t<allocator_t>, allocator_t>;
-
-    std::cout << "sizeof(): " << sizeof(queue_t) << std::endl;
-
     return test_main<queue_t, perf_allocated_test<allocator_t>>(argc, argv);
 }
