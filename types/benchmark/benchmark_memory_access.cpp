5e6ab865812acd4cf5735223e3596bfa5b6fc500
Evgeny Proydakov <e.proydakov@gmail.com>
2022-10-19 13:14:27 +0300

    Added benchmark_memory_access for reference data, global data, thread local data.

---
+++ types/benchmark/benchmark_memory_access.cpp
@@ -0,0 +1,60 @@
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+#include <catch2/catch.hpp>
+
+#include <compiler/compiler.h>
+
+#include <functional>
+
+namespace
+{
+int64_t g_counter{};
+}
+
+IHFT_NOINLINE int64_t accumulate_global()
+{
+    g_counter++;
+    return g_counter;
+}
+
+TEST_CASE("global_variable benchmark")
+{
+    BENCHMARK("accumulate_global()")
+    {
+        return accumulate_global();
+    };
+}
+
+namespace
+{
+thread_local int64_t g_tl_counter{};
+}
+
+IHFT_NOINLINE int64_t accumulate_thread_local()
+{
+    g_tl_counter++;
+    return g_tl_counter;
+}
+
+TEST_CASE("thread_local_variable benchmark")
+{
+    BENCHMARK("accumulate_thread_local()")
+    {
+        return accumulate_thread_local();
+    };
+}
+
+IHFT_NOINLINE int64_t accumulate_by_reference(int64_t& ref)
+{
+    ref++;
+    return ref;
+}
+
+TEST_CASE("stack_local_variable benchmark")
+{
+    int64_t var{};
+    BENCHMARK("accumulate_be_reference()")
+    {
+        return accumulate_by_reference(var);
+    };
+}
+
