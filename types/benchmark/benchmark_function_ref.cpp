87cd26e159fb2c380e0c7ef41a4c983ad183d65f
Evgeny Proydakov <e.proydakov@gmail.com>
2023-03-12 00:39:08 +0300

    Review types function_ref benchmark for compact lambda

--- types/benchmark/benchmark_function_ref.cpp
+++ types/benchmark/benchmark_function_ref.cpp
@@ -57,11 +57,52 @@ TEST_CASE("call std_function for plain_function benchmark")
 
 namespace
 {
+    // complex lambda function with compact storage content
+
+    constexpr uint64_t c{};
+
+    static auto compact_lambda = [v1 = c, v2 = c, v3 = c]
+        (uint64_t& o, uint64_t d) mutable -> uint64_t
+    {
+        v1 += d;
+        v2 += d;
+        v3 += d;
+
+        o += v1 + v2 + v3;
+
+        return 0;
+    };
+}
+
+TEST_CASE("construct + call function_ref for compact_lambda benchmark")
+{
+    uint64_t counter = 0;
+
+    BENCHMARK("construct function_ref()")
+    {
+        function_ref plaing_function_ref{compact_lambda};
+        return plaing_function_ref(counter, 1);
+    };
+}
+
+TEST_CASE("construct + call std_function for compact_lambda benchmark")
+{
+    uint64_t counter = 0;
+
+    BENCHMARK("construct std_function()")
+    {
+        function_std plain_std_function{compact_lambda};
+        return plain_std_function(counter, 1);
+    };
+}
+
+namespace
+{
     // complex lambda function with huge storage content
 
-    constexpr uint64_t v{};
+    constexpr uint64_t h{};
 
-    static auto huge_lambda = [v1 = v, v2 = v, v3 = v, v4 = v, v5 = v, v6 = v, v7 = v, v8 = v]
+    static auto huge_lambda = [v1 = h, v2 = h, v3 = h, v4 = h, v5 = h, v6 = h, v7 = h, v8 = h]
         (uint64_t& o, uint64_t d) mutable -> uint64_t
     {
         v1 += d;
